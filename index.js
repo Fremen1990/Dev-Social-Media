0251105cdd6729d694c8092cea8b7afa7144956e
fremen1990 <fremen1990@gmail.com>
2022-06-06 23:14:05 +0200

    Middlewares express.json, helmet and morgan added, first request tested.

--- index.js
+++ index.js
@@ -11,8 +11,23 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
   console.log("Connected to MongoDB");
 });
 
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to homepage");
+});
+
+app.get("/users", (req, res) => {
+  res.send("Users page");
+});
+
 const port = 8800;
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Backend server is running on port ${port}....`);
 });
+
+//TODO 8:10 min Middleware and first request
